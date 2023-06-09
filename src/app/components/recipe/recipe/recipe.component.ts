5b8559e72d17ef89b3d2a666db247eea87a3d150
Teodora Smaranda <teodora.smaranda@emea.nttdata.com>
2023-06-09 20:05:03 +0300

    dupa ce am reparat proj

--- src/app/components/recipe/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe/recipe.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { RecipeService } from 'src/app/services/recipe.service';
 import { Recipe } from 'src/app/types/recipe';
 import { RecipeDialogComponent } from '../../recipe-dialog/recipe-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe',
@@ -15,60 +17,70 @@ export class RecipeComponent implements OnInit {
   editMode = false;
   recipeForm!: FormGroup;
   recipeData!: Recipe[];
-  recipe!: Recipe;
+  data!: Recipe;
   index!: string;
   imagePreview!: string;
+  // recipes: Recipe[];
+   subscription!: Subscription;
 
   constructor(
     private fb: FormBuilder,
     private recipeService: RecipeService,
-    public dialog: MatDialog
-  ) {}
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
+  )
+  {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    // this.subscription = this.recipeService.recipesChanged
+    //   .subscribe(
+    //     (recipes: Recipe[]) => {
+    //       this.recipes
+    //   );
+    // this.recipes = this.recipeService.getRecipe();
+  }
 
   onLoadRecipeDetail() {
-    if (this.recipe && this.recipe.id) {
-      this.recipeService.getRecipeById(this.recipe.id).subscribe((recipe) => {
-        console.log(recipe);
+    if (this.data && this.data.id) {
+      this.recipeService.getRecipeById(this.data.id).subscribe((data) => {
+        console.log(data);
       });
     }
   }
 
   openDialog() {
-    let dialogRef: MatDialogRef<RecipeDialogComponent>;
-    if (this.isRecipe) {
-      dialogRef = this.dialog.open(RecipeDialogComponent, {
-        height: '100%',
-        width: '660px',
-        position: { right: '0px', top: '2px', bottom: '0px' },
-      });
-    }
+    const dialogRef = this.dialog.open(RecipeDialogComponent, {
+      height: '100%',
+      width: '660px',
+      position: { right: '0px', top: '2px', bottom: '0px' },
+      data: {
+        id: '',
+        recipe_title: 'Prajitura',
+        image: '',
+        ingredients: '- lapte, oua, faina',
+        preparation:
+          'Topiți untul. Separati albușurile de gălbenușuri. Mixați gălbenușurile cu zahărul până obțineți o cremă. Adăugați untul topit și răcit și amestecați. Cerneți făina și adăugați-o treptat. Turnați laptele și esența de vanilie și amestecați. Mixați albușurile cu sare până devin spumă. Incorporați spuma de albușuri în compoziție. Turnați în tavă și coaceți. Lăsați să se răcească și presărați zahăr pudră. Portionați și serviți.',
+        complexity: 'low',
+        preparation_time: '15 minute',
+        baking_time: '1 ora',
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((res) => {
+      if (res.recipe_title) {
+        this.recipeService
+          .createRecipe(res)
+          .then((data: any) => console.log(data));
+      }
+    });
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], { relativeTo: this.route });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
-
-// recipes: Recipe[];
-// subscription: Subscription;
-
-// constructor(private recipeService: RecipeService,
-//             private router: Router,
-//             private route: ActivatedRoute) {
-// }
-
-// ngOnInit() {
-//   this.subscription = this.recipeService.recipesChanged
-//     .subscribe(
-//       (recipes: Recipe[]) => {
-//         this.recipes = recipes;
-//       }
-//     );
-//   this.recipes = this.recipeService.getRecipes();
-// }
-
-// onNewRecipe() {
-//   this.router.navigate(['new'], {relativeTo: this.route});
-// }
-
-// ngOnDestroy() {
-//   this.subscription.unsubscribe();
-// }
